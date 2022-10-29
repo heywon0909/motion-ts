71736636efe04c4fd7537fe44374e60253f6b0ba
heywon0909 <hw7319@naver.com>
2022-10-29 14:04:02 +0900

    삭제기능 구현 따라해보기

--- src/app.ts
+++ src/app.ts
@@ -2,25 +2,27 @@ import { TodoComponent } from './components/page/item/todo.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { VideoComponent } from './components/page/item/video.js';
 import { ImageComponent } from './components/page/item/image.js';
-import { PageComponent } from './components/page/page.js';
+import { Composable, PageComponent } from './components/page/page.js';
+import { Component } from './components/Component.js';
 
 class App{
-    private readonly page: PageComponent;
+    private readonly page: Component & Composable;
     constructor(appRoot: HTMLElement) {
         this.page = new PageComponent();
         this.page.attachTo(appRoot);
 
         const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
-        image.attachTo(appRoot, 'beforeend');
+        this.page.addChild(image);
+       
 
         const video = new VideoComponent('video title', 'https://www.youtube.com/watch?v=C87fr5Q12aY');
-        video.attachTo(appRoot, 'beforeend');
+        this.page.addChild(video);
 
         const note = new NoteComponent('note title', 'note Body');
-        note.attachTo(appRoot, 'beforeend');
+        this.page.addChild(note);
 
         const todo = new TodoComponent('Todo Title', 'Todo Item');
-        todo.attachTo(appRoot, 'beforeend');
+        this.page.addChild(todo);
     }
 }
 
