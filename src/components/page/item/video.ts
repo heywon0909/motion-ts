67d486ee349f968ab0a65fdb0ea28da91cbf8579
heywon0909 <hw7319@naver.com>
2022-10-29 10:40:51 +0900

    정규표현식 x-frame-options로 인하여 이전 함수로 변경

--- src/components/page/item/video.ts
+++ src/components/page/item/video.ts
@@ -8,26 +8,26 @@ export class VideoComponent extends BaseComponent<HTMLElement>{
                 </section>`)
         
         const iframe = this.element.querySelector('.video__iframe')! as HTMLIFrameElement;
-        // iframe.src = `https://www.youtube.com/embed/${this.extractVideoID(url)}`;
-        iframe.src = this.converToEmbeddedURL(url);
+        iframe.src = `https://www.youtube.com/embed/${this.extractVideoID(url)}`;
+        // iframe.src = this.converToEmbeddedURL(url);
         const titleElement = this.element.querySelector('.video__title')! as HTMLHeadElement;
         titleElement.textContent = title;
     }
     //정규표현식 Regex 
-    private converToEmbeddedURL(url: string): string {
-        const regExp = /^(?: https ?: \/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
-        const match = url.match(regExp);
-        const videoId = match ? match[1] || match[2] : undefined;
-        if (videoId) {
-            return `https://www.youtube.com/embed/${videoId}`;
-        }
-        return url;
+    // private converToEmbeddedURL(url: string): string {
+    //     const regExp = /^(?: https ?: \/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+    //     const match = url.match(regExp);
+    //     const videoId = match ? match[1] || match[2] : undefined;
+    //     if (videoId) {
+    //         return `https://www.youtube.com/embed/${videoId}`;
+    //     }
+    //     return url;
 
     // }
-    // private extractVideoID(url: string): string {
-    //     const filter = url.split("v=")[1]!.split("&")[0];
-    //     return filter! as string;
-    // }
+    private extractVideoID(url: string): string {
+        const filter = url.split("v=")[1]!.split("&")[0];
+        return filter! as string;
+    }
 }
 
 
